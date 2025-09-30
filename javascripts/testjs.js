4c75e4a0db11e3b7fc907d79a3bef46691896e46
prylfuk <parunyu.saet@gmail.com>
2025-09-30 18:11:40 +0700

    add js check number

--- javascripts/testjs.js
+++ javascripts/testjs.js
@@ -1,1 +1,9 @@
-alert('in js folder');
+let num = parseInt(prompt("Input integer number"));
+
+if (num === 0) {
+    alert("zero integer number");
+} else if (num % 2 === 0) {
+    alert("even integer number");
+} else {
+    alert("odd integer number");
+}
